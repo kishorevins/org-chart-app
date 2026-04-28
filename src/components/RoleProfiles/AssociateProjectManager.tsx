d0867489a92d1adb79a4bed3fd1633e997ee0d1a
kishore.s <talktokishoresolairaj@gmail.com>
2026-04-28 19:17:40 +0530

    added and modified roles and responsiblity

---
+++ src/components/RoleProfiles/AssociateProjectManager.tsx
@@ -0,0 +1,11 @@
+﻿const AssociateProjectmanager = () => {
+  return (
+    <iframe
+      src="/role-profiles/Associate-Project-manager.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="Associate-Project-manager"
+    />
+  );
+};
+
+export default AssociateProjectmanager;
